6fd4c1d60a7234fb35276c47c2176c997a79de9d
mattetress <mattetress@me.com>
2021-06-14 21:16:38 -0500

    set up slider

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -2,8 +2,28 @@ const data = [
   {
     id: 1,
     types: ["Java"],
-    languages: ["java", "rails", "ruby", "html", "javascript"],
+    languages: ["java", "mysql"],
     title: "Scheduling System",
+    shortDesc: "JavaFX application which provides GUI for managing appointments in database.",
+    fullDesc: [
+      "JavaFX application for WGU Software II that provides a GUI for members of an organization to manage customers and appointments in their database.",
+      "Uses JDBC to connect with a MySQL database and displays data in tables and forms. "
+    ],
+    splashImage: "/assets/arthub-splash.png",
+    largeImage: "/assets/arthub-full.png",
+    githubURL: "https://github.com/RubyMineshaft/arthub-rails",
+    demoURL: null,
+    bullets: [
+      "Leverages resource bundles to automatically translate based on system default.",
+      "Utilizes Lambda expressions for more efficient lines of code.",
+      "Converts times from UTC to system time for display. Validates against EST business hours before creating or updating records."
+    ]
+  },
+  {
+    id: 2,
+    types: ["Java", "Ruby"],
+    languages: ["java", "rails", "ruby", "html", "javascript"],
+    title: "Number 2",
     shortDesc: "JavaFX application which provides GUI for managing appointments in database.",
     fullDesc: [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum vitae eum magnam ratione quam omnis reprehenderit nobis, repellendus, minus, eveniet ex at ducimus facilis hic dolor. Nostrum ratione ad eum!",
@@ -21,31 +41,10 @@ const data = [
     ]
   },
   {
-    id: 1,
-    types: ["Java"],
+    id: 3,
+    types: ["Java", "JavaScript"],
     languages: ["java", "rails", "ruby", "html", "javascript"],
-    title: "Scheduling System",
-    shortDesc: "JavaFX application which provides GUI for managing appointments in database.",
-    fullDesc: [
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum vitae eum magnam ratione quam omnis reprehenderit nobis, repellendus, minus, eveniet ex at ducimus facilis hic dolor. Nostrum ratione ad eum!",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum vitae eum magnam ratione quam omnis reprehenderit nobis, repellendus, minus, eveniet ex at ducimus facilis hic dolor. Nostrum ratione ad eum!"
-    ],
-    splashImage: "/assets/arthub-splash.png",
-    largeImage: "/assets/arthub-full.png",
-    githubURL: "https://github.com/RubyMineshaft/arthub-rails",
-    demoURL: null,
-    bullets: [
-      "Implemented OAuth authentication strategy with Omniauth-Facebook gem",
-      "Leveraged jQuery and JavaScript for AJAX functionality",
-      "Configured Rails API with ActiveModel Serializers.",
-      "Managed attachments with Active Storage and AWS S3 Bucket."
-    ]
-  },
-  {
-    id: 1,
-    types: ["Java"],
-    languages: ["java", "rails", "ruby", "html", "javascript"],
-    title: "Scheduling System",
+    title: "Third App",
     shortDesc: "JavaFX application which provides GUI for managing appointments in database.",
     fullDesc: [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum vitae eum magnam ratione quam omnis reprehenderit nobis, repellendus, minus, eveniet ex at ducimus facilis hic dolor. Nostrum ratione ad eum!",
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAllProjects } from '../../ProjectData';
 import Project from './Project';
 import "./projects.scss";
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 
 export default function Projects() {
 
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  const handleClick = (dir) => {
+    if (dir === "back") {
+      setCurrentSlide(currentSlide > 1 ? currentSlide - 1 : getAllProjects().length);
+    } else {
+      setCurrentSlide(currentSlide === getAllProjects().length ? 1 : currentSlide + 1);
+    }
+  }
+
   return (
     <div className="projects" id="projects">
-      <div className="slider">
+      <div className="slider" style={{transform: `translateX(-${(currentSlide - 1) * 100}vw)`}} >
         
-          {getAllProjects().map(project => <Project project={project} />)}
-        </div>
+        {getAllProjects().map(project => <Project project={project} />)}
       </div>
-  
+      <NavigateNextIcon className="next" onClick={() => handleClick("next")} />
+      <NavigateBeforeIcon className="back" onClick={() => handleClick("back")} />
+      </div>
   )
 }
